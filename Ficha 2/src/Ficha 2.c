d23f76f46931be4019032cb5175e3481307ff0a9
helder2075022 <heldergoncalves53@gmail.com>
2022-10-14 19:49:38 +0100

    Ficha 2

---
+++ Ficha 2/src/Ficha 2.c
@@ -0,0 +1,69 @@
+/*
+ ============================================================================
+ Name        : Ficha.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(void) {
+
+	//Exercício 1:
+
+	float p1 = 13.0;
+	float p2 = 14.0;
+	float freq = 12.0;
+
+	float avaliacao = (p1 * 0.3) + (p2 * 0.3) + (freq * 0.4);
+	printf("avaliacao final é %f \n", avaliacao);
+
+	// Exercício 2:
+
+	float base = 5.0;
+	float altura = 10.0;
+
+	float area = base * altura;
+
+	printf("A área do retangulo é %f \n", area);
+
+	// Exercício 3:
+
+	float comprimento = 20.0;
+	float largura = 15.0;
+	float altura2 = 10.0;
+    float volume = comprimento * largura * altura2;
+    printf("O volume do pararalelepípedo é %f \n", volume);
+
+    // Exercício 4:
+
+    float quilometros = 300.0;
+    float litros = 20;
+    float consumo = (litros/quilometros*100);
+
+    printf("Consumo médio é %fl/100KM \n", consumo);
+
+    // Exercício 5:
+
+    float ff = sqrt(4);
+
+    float exp = pow(2,3);
+
+    printf("%f", exp);
+
+
+
+
+
+
+
+
+
+	return EXIT_SUCCESS;
+}
+\
---
+++ Ficha1/src/Ficha1.c
@@ -0,0 +1,50 @@
+/*
+ ============================================================================
+ Name        : Ficha1.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	float pi = 3.14;
+	float raio = 2.5;
+	float area = pi * raio * raio;
+
+	// Exercício 3:
+
+	int a = 5, b = 3, c = 7, d = 10, e = 1;
+	float media = (a + b + c + d + e) / 5.0;
+	printf("%f \n", media);
+
+	// Exercício 4:
+
+	float fahr = 212;
+	float celsius = (fahr - 32) / 1.8;
+	printf("%f fahrenheit são %f celsius \n", fahr, celsius);
+
+	// Exercício 5:
+
+	int horas = 8;
+	int minutos = 22;
+	int segundos = 10;
+	int total = horas * 3600 + min * 60 + segundos;
+
+	float segundos = 7231;
+	float minutos = segundos / 60;
+	float horas = minutos / 60;
+
+	printf(Total%f,total);
+
+
+
+
+
+
+	return EXIT_SUCCESS;
+}
