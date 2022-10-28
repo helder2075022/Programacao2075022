00992dd4a3a7c791b3fff6e2059110cce4709f6c
helder2075022 <heldergoncalves53@gmail.com>
2022-10-28 18:00:55 +0100

    Ficha3

--- ficha3/src/ficha3.c
+++ ficha3/src/ficha3.c
@@ -1,6 +1,6 @@
 /*
  ============================================================================
- Name        : ficha3.c
+ Name        : Ficha3.c
  Author      : 
  Version     :
  Copyright   : Your copyright notice
@@ -10,121 +10,94 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
-//=======================================Exercício 1===================
-
+#include <math.h>
 
 int main(void) {
+
+	//===============================================================Exercício 4========================================================
+
 	/*
-
-   float p1 = 14.0;
-   float p2 =  12.0;
-   float freq = 13.0;
-
-   float  media = (p1*0.3) + (p2*0.3) + (freq*0.4);
-
-    if (media > 9.5) {
-    	printf("%f\n aluno está aprovado", media);
-    } else (media < 9.5) {
-    	printf("%f\n aluno está reprovado",media);
-    }
-    */
-
-    //========================================Exercício 2====================
-   /*
-	puts("insira a altura");
-    int altura;
-    scanf("%i", &altura);
-
-    if (altura > 180) {
-       printf("alta estatura");
-    } else if(altura > 165) {
-    	printf("media estatura");
-    }else {
-    	printf("baixa estatura");
-    }
-
-    */
-
-	//========================================Exercício 3======================
-/*
-	int mes = 3;
-
-	if(mes == 1){
-		puts("Janeiro");
-	} else if(mes == 2) {
-		puts("Fevereiro");
-	}else if(mes == 3) {
-		puts("Marco");
-	}else if(mes == 4) {
-		puts("Abril");
-	}else if(mes == 5) {
-		puts("Maio");
-	}else if(mes == 6) {
-		puts("Junho");
-	}else if(mes == 7) {
-		puts("Julho");
-	}else if(mes == 8) {
-		puts("Agosto");
-	}else if(mes == 9) {
-		puts("Setembro");
-	}else if(mes == 10) {
-		puts("Outubro");
-	}else if(mes == 11) {
-		puts("Novembro");
-	}else(mes == 12) {
-		puts("Dezembro");
-	}
-
-	*/
-
-//==============================================Exercício 4=====================
 	float operando1 = 2;
 	float operando2 = 4;
-	char operador = '-';
+	char operador = '+';
 
 	float resultado = 0;
 
-	if(operador == '+'){
-		resultado = operando1 + operando2;
-	}
-	else if(operador == '-'){
+	if(operador == '+') {
+		resultado = operando1 + operando2:
+	}else if(operador == '-') {
 		resultado = operando1 - operando2;
-	}
-	else if(operador == '*'){
+	}else if(operador == '*') {
 		resultado = operando1 * operando2;
-	}
-	else(operador == '/'){
+	}else if(operador == '/') {
 		resultado = operando1 / operando2;
+	}else if(operador == '^') {
+		resultado = operando1, operando2;
 	}
 
-	printf("%f %c %f = %f",operando1, operador, operando2,resultado);
+	printf("%.2f %c &.1f = %.1f",operando1, operador, operando2, resultado);
+	*/
+	//===============================================================Exercício 5=============================================================
+/*
 
 
+    int  soma = 0;
+	int max = 100;
+	int i = 1;
 
 
+	while(i <= max){
+		soma = soma + i;
+		i++;
+	}
 
+	printf("Soma até %i é: %i", 100, soma);
+*/
+//===================================================================Exercício 6=============================================================
+/*
+	int multiplo = 5;
+	int max = 20;
+	int i = 0;
 
+	printf("%i \n", 1);
+	while(i  < max){
+		i += multiplo;
+		printf( "%i \n",i);
+	}
+	*/
 
+	/*
+	    int multiplo = 5;
+		int max = 20;
+		int index = 1;
 
+		while(index  < max){
+			if(index % multiplo == 0){
+				printf( "%i \n",index);
+			}
+			index++;
+		}
+		*/
 
+//===================================================================Exercício 7=============================================================
+/*
+int n = 5;
+int factorial = 1;
 
+while(n >= 1){
+	factorial = factorial * n;
+	n--;
+}
+printf("%i! é: %i \n", 5, factorial);
+*/
+//==================================================================Exercício 8=========================================================
 
+	int ano = 1999;
 
+	if(ano / 4) {
+		ano = bissexto;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+	}else
 
 	return EXIT_SUCCESS;
 }
